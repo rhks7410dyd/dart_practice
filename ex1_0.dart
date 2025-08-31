d2f4c0c2a0b2b515277e4b3842eabba49575b0d5
rhks7410dyd <rhks7410dyd@naver.com>
2025-08-31 17:50:12 +0900

    add 1_1,1_2

---
+++ ex1_0.dart
@@ -0,0 +1,7 @@
+void main(){
+  print('hello world');
+}
+
+/*
+main은 엔트리 포인트로서 일반적 역할을 수행함
+ */
---
+++ ex1_1.dart
@@ -0,0 +1,8 @@
+void main(){
+  var name = 'Test';
+  String name2 = 'Test2';
+}
+/*
+관습적으로 함수나 메소드 내부에서는 지역변수로 var을 사용함
+class에서 변수나 property는 명시적으로 선언
+*/
